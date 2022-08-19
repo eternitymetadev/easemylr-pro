f5413dc04c4f71593ecde0fb11aa701659f1f5e6
vikaschandel <vikassingh438>
2022-08-19 17:32:41 +0530

    new drs static

--- app/Helpers/GlobalFunctions.php
+++ app/Helpers/GlobalFunctions.php
@@ -142,7 +142,7 @@ class GlobalFunctions {
         return $status;
      }
 
-     public function oldnewLr($drs_number)
+     public static function oldnewLr($drs_number)
      {
         $transcationview = TransactionSheet::with('ConsignmentDetail')->where('drs_no', $drs_number)->first();
              $orderId = $transcationview->ConsignmentDetail->order_id;
