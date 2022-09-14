93b631b4a6b1a42fbb395fa06c5daf760dd4baef
unknown <vikassingh438@gmail.com>
2022-09-14 12:05:51 +0530

    add bu check data

--- app/Http/Controllers/ConsignmentController.php
+++ app/Http/Controllers/ConsignmentController.php
@@ -15,6 +15,7 @@ use App\Models\Role;
 use App\Models\TransactionSheet;
 use App\Models\Vehicle;
 use App\Models\VehicleType;
+use App\Models\User;
 use Auth;
 use DataTables;
 use DB;
@@ -97,6 +98,8 @@ class ConsignmentController extends Controller
         $regclient = explode(',',$authuser->regionalclient_id);
         $cc = explode(',',$authuser->branch_id);
 
+        $user = User::where('branch_id',$authuser->branch_id)->where('role_id',2)->first();
+
         $data = DB::table('consignment_notes')->select('consignment_notes.*', 'consigners.nick_name as consigner_id', 'consigners.city as con_city', 'consigners.postal_code as con_pincode', 'consigners.district as con_district', 'consignees.nick_name as consignee_id', 'consignees.city as city', 'consignees.postal_code as pincode', 'consignees.address_line1 as con_add1', 'consignees.address_line2 as con_add2', 'consignees.address_line3 as con_add3','consignees.district as conee_district', 'vehicles.regn_no as regn_no','drivers.name as driver_name', 'drivers.phone as driver_phone', 'jobs.status as job_status', 'jobs.response_data as trail')
             ->join('consigners', 'consigners.id', '=', 'consignment_notes.consigner_id')
             ->join('regional_clients', 'regional_clients.id', '=', 'consigners.regionalclient_id')
@@ -112,7 +115,7 @@ class ConsignmentController extends Controller
                 $data;
             }
             elseif($authuser->role_id ==4){
-                $data = $data->where('consignment_notes.user_id', $authuser->id);
+                $data = $data->where('consignment_notes.user_id', $authuser->id)->orWhere('consignment_notes.user_id', $user->id);
             }
             elseif($authuser->role_id ==6){
                 $data = $data->whereIn('base_clients.id', $baseclient);
