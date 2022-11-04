af231e2c60b077819d6299048dd91883451df35f
vikaschandel <vikassingh438>
2022-11-04 13:47:52 +0530

    payment pending

--- app/Http/Controllers/VendorController.php
+++ app/Http/Controllers/VendorController.php
@@ -742,6 +742,7 @@ class VendorController extends Controller
         $role_id = Role::where('id', '=', $authuser->role_id)->first();
         $cc = $authuser->branch_id;
         $user = $authuser->id;
+        $bm_email = $authuser->email;
 
         $branch_name = Location::where('id', '=', $request->branch_id)->first();
 
@@ -766,11 +767,13 @@ class VendorController extends Controller
         }
 
         $i = 0;
+        $sent_vehicle = array();
         foreach ($simplyfy as $value) {
             $i++;
             $drs_no = $value['drs_no'];
             $vendor_id = $request->vendor_name;
             $vehicle_no = $value['vehicle_no'];
+            $sent_vehicle[] = $value['vehicle_no'];
 
             if ($request->p_type == 'Advance') {
                 $balance_amt = $request->claimed_amount - $request->pay_amt;
@@ -790,6 +793,7 @@ class VendorController extends Controller
             }
 
         }
+        $sent_venicle_no = implode(',', $sent_vehicle);
 
         TransactionSheet::whereIn('drs_no', $drsno)->update(['request_status' => '1']);
         // ============== Sent to finfect
@@ -816,9 +820,11 @@ class VendorController extends Controller
             \"claimed_amount\": \"$request->claimed_amount\",
             \"pfu\": \"$pfu\",
             \"ptype\": \"$request->p_type\",
-            \"email\": \"$request->email\",
+            \"email\": \"$bm_email\",
             \"terid\": \"$transaction_id_new\",
             \"branch\": \"$branch_name->nick_name\",
+            \"vehicle\": \"$sent_venicle_no\",
+            \"pan\": \"$request->pan\",
             \"txn_route\": \"DRS\"
             }]",
             CURLOPT_HTTPHEADER => array(
@@ -849,7 +855,7 @@ class VendorController extends Controller
 
                 PaymentRequest::where('transaction_id', $transaction_id_new)->update(['payment_type' => $request->p_type, 'advanced' => $advance, 'balance' => $balance, 'payment_status' => 2]);
 
-                $bankdetails = array('acc_holder_name' => $request->beneficiary_name, 'account_no' => $request->acc_no, 'ifsc_code' => $request->ifsc, 'bank_name' => $request->bank_name, 'branch_name' => $request->branch_name, 'email' => $request->email);
+                $bankdetails = array('acc_holder_name' => $request->beneficiary_name, 'account_no' => $request->acc_no, 'ifsc_code' => $request->ifsc, 'bank_name' => $request->bank_name, 'branch_name' => $request->branch_name, 'email' => $bm_email);
 
                 $paymentresponse['refrence_transaction_id'] = $res_data->refrence_transaction_id;
                 $paymentresponse['transaction_id'] = $transaction_id_new;
@@ -869,7 +875,7 @@ class VendorController extends Controller
 
                 $balance_amt = $request->claimed_amount - $request->pay_amt;
                 //======== Payment History save =========//
-                $bankdetails = array('acc_holder_name' => $request->beneficiary_name, 'account_no' => $request->acc_no, 'ifsc_code' => $request->ifsc, 'bank_name' => $request->bank_name, 'branch_name' => $request->branch_name, 'email' => $request->email);
+                $bankdetails = array('acc_holder_name' => $request->beneficiary_name, 'account_no' => $request->acc_no, 'ifsc_code' => $request->ifsc, 'bank_name' => $request->bank_name, 'branch_name' => $request->branch_name, 'email' => $bm_email);
 
                 $paymentresponse['refrence_transaction_id'] = $res_data->refrence_transaction_id;
                 $paymentresponse['transaction_id'] = $transaction_id_new;
