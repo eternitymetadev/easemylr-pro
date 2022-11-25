e8511c69dbe45510d3e835fa38ffc6e80b733d1c
unknown <vikassingh438@gmail.com>
2022-11-25 12:13:11 +0530

    add commit

--- app/Http/Controllers/ConsignmentController.php
+++ app/Http/Controllers/ConsignmentController.php
@@ -116,7 +116,7 @@ class ConsignmentController extends Controller
                     //     });
                     // });
 
-                });
+                // });
             }
 
             if ($request->peritem) {
