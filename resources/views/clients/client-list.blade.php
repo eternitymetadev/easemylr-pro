12e4623d570b9e06898a873d0702db6c1d230bb1
thakuramit962 <thakuramit962@gmail.com>
2022-12-15 14:55:07 +0530

    clients menu UI done

--- resources/views/clients/client-list.blade.php
+++ resources/views/clients/client-list.blade.php
@@ -1,104 +1,71 @@
 @extends('layouts.main')
 @section('content')
-<!-- BEGIN PAGE LEVEL CUSTOM STYLES -->
+    <!-- BEGIN PAGE LEVEL CUSTOM STYLES -->
     <link rel="stylesheet" type="text/css" href="{{asset('plugins/table/datatable/datatables.css')}}">
     <link rel="stylesheet" type="text/css" href="{{asset('plugins/table/datatable/custom_dt_html5.css')}}">
     <link rel="stylesheet" type="text/css" href="{{asset('plugins/table/datatable/dt-global_style.css')}}">
-<!-- END PAGE LEVEL CUSTOM STYLES -->
-<style>
-<style>
-        .dt--top-section {
-    margin:none;
-}
-div.relative {
-    position: absolute;
-    left: 110px;
-    top: 24px;
-    z-index: 1;
-    width: 83px;
-    height: 38px;
-}
-div.relat {
-    position: absolute;
-    left: 110px;
-    top: 24px;
-    z-index: 1;
-    width: 240px;
-    height: 38px;
-}
+    <!-- END PAGE LEVEL CUSTOM STYLES -->
 
-
-/* .table > tbody > tr > td {
-    color: #4361ee;
-} */
-.dt-buttons .dt-button {
-    width: 83px;
-    height: 38px;
-    font-size: 13px;
-}
-.btn-group > .btn, .btn-group .btn {
-    padding: 0px 0px;
-    padding: 10px;
-}
-.btn {
-   
-    font-size: 10px;
-    }
-    </style>
-    </style>
 
     <div class="layout-px-spacing">
-        <div class="row layout-top-spacing">
-            <div class="col-xl-12 col-lg-12 col-sm-12  layout-spacing">
-                <div class="page-header">
-                    <nav class="breadcrumb-one" aria-label="breadcrumb">
-                        <ol class="breadcrumb">
-                            <li class="breadcrumb-item"><a href="javascript:void(0);">Clients</a></li>
-                            <li class="breadcrumb-item active" aria-current="page"><a href="javascript:void(0);">Client List</a></li>
-                        </ol>
-                    </nav>
-                </div>
-                <div class="widget-content widget-content-area br-6">
-                    <div style="margin-left:9px;" class="page-breadcrumb d-none d-sm-flex align-items-center mb-3">
-                        <div class="ms-auto">
-                        </div>
-                    </div>
-                    <div class="table-responsive mb-4 mt-4">
-                        @csrf
-                        <table id="clienttable" class="table table-hover get-datatable" style="width:100%">
-                        <div class="btn-group relat">
-                        <a href="{{'clients/create'}}" class="btn btn-primary pull-right" >Create Client</a>
-                        <a href="{{'reginal-clients'}}" class="btn btn-primary pull-right" style="margin-left:7px;">Regional Client List</a>
-                        </div>
-                            <thead>
-                                <tr>
-                                    <th>Sr No.</th>
-                                    <th>Name</th>
-                                    <th>Action</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <?php 
-                                if(count($clients)>0) {
-                                    foreach ($clients as $key => $value) {  
-                                ?> 
-                                <tr>
-                                    <td>{{ ++$i }}</td>
-                                    <td>{{ ucwords($value->client_name ?? "-")}}</td>
-                                    <td>
-                                        <a class="btn btn-primary" href="{{url($prefix.'/clients/'.Crypt::encrypt($value->id).'/edit')}}" ><span><i class="fa fa-edit"></i></span></a>
-                                    </td>   
-                                    
-                                </tr>
-                                <?php 
-                                    }
-                                } ?>
-                            </tbody>
-                        </table>
-                        
-                    </div>
-                </div>
+        <div class="page-header layout-spacing">
+            <h2 class="pageHeading">Client List</h2>
+
+            <div class="d-flex align-items-center" style="gap: 1rem;">
+                <a href="{{'clients/create'}}"
+                   class="btn btn-primary">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                         fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                         stroke-linejoin="round" class="feather feather-plus">
+                        <line x1="12" y1="5" x2="12" y2="19"></line>
+                        <line x1="5" y1="12" x2="19" y2="12"></line>
+                    </svg>
+                    Client
+                </a>
+                <a href="{{'reginal-clients'}}"
+                   class="btn btn-primary">
+                    Regional Client
+                </a>
             </div>
         </div>
+
+
+        <div class="widget-content widget-content-area br-6">
+            <div class="table-responsive mb-4 mt-4">
+                @csrf
+                <table id="clienttable" class="table table-hover get-datatable" style="width:100%">
+                    <thead>
+                    <tr>
+                        <th style="width: 100px">Sr No.</th>
+                        <th>Name</th>
+                        <th style="text-align: center">Action</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    @if(count($clients) > 0)
+                        @foreach ($clients as $key => $value)
+                            <tr>
+                                <td style="width: 100px">{{ ++$i }}</td>
+                                <td>{{ ucwords($value->client_name ?? "-")}}</td>
+                                <td style="text-align: center">
+                                    <a class="text-center edit editIcon mx-auto"
+                                       href="{{url($prefix.'/clients/'.Crypt::encrypt($value->id).'/edit')}}">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                             viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                                             stroke-linecap="round" stroke-linejoin="round"
+                                             class="feather feather-edit">
+                                            <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path>
+                                            <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path>
+                                        </svg>
+                                    </a>
+                                </td>
+                            </tr>
+                        @endforeach
+                    @endif
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
     </div>
 @endsection
--- resources/views/clients/regional-client-list.blade.php
+++ resources/views/clients/regional-client-list.blade.php
@@ -1,94 +1,72 @@
 @extends('layouts.main')
 @section('content')
-<!-- BEGIN PAGE LEVEL CUSTOM STYLES -->
+    <!-- BEGIN PAGE LEVEL CUSTOM STYLES -->
     <link rel="stylesheet" type="text/css" href="{{asset('plugins/table/datatable/datatables.css')}}">
     <link rel="stylesheet" type="text/css" href="{{asset('plugins/table/datatable/custom_dt_html5.css')}}">
     <link rel="stylesheet" type="text/css" href="{{asset('plugins/table/datatable/dt-global_style.css')}}">
-<!-- END PAGE LEVEL CUSTOM STYLES -->
-<style>
-.dt--top-section {
-    margin:none;
-}
-div.relative {
-    position: absolute;
-    left: 110px;
-    top: 24px;
-    z-index: 1;
-    width: 83px;
-    height: 38px;
-}
-/* .table > tbody > tr > td {
-    color: #4361ee;
-} */
-.dt-buttons .dt-button {
-    width: 83px;
-    height: 38px;
-    font-size: 13px;
-}
-.btn-group > .btn, .btn-group .btn {
-    padding: 0px 0px;
-    padding: 10px;
-}
-.btn {
-    font-size: 10px;
-    }
-</style>
+    <!-- END PAGE LEVEL CUSTOM STYLES -->
 
     <div class="layout-px-spacing">
-        <div class="row layout-top-spacing">
-            <div class="col-xl-12 col-lg-12 col-sm-12  layout-spacing">
-                <div class="page-header">
-                    <nav class="breadcrumb-one" aria-label="breadcrumb">
-                        <ol class="breadcrumb">
-                            <li class="breadcrumb-item"><a href="{{'clients'}}">Clients</a></li>
-                            <li class="breadcrumb-item active" aria-current="page"><a href="javascript:void(0);">Regional Client List</a></li>
-                        </ol>
-                    </nav>
-                </div>
-                <div class="widget-content widget-content-area br-6">
-                    <div style="margin-left:9px;" class="page-breadcrumb d-none d-sm-flex align-items-center mb-3">
-                        <div class="ms-auto">
-                        </div>
-                    </div>
-                    <div class="table-responsive mb-4 mt-4">
-                        @csrf
-                        <table id="clienttable" class="table table-hover get-datatable" style="width:100%">
-                        <div class="btn-group relative">
-                        
-                        </div>
-                            <thead>
-                                <tr>
-                                    <th>Id</th>
-                                    <th>Name</th>
-                                    <th>Location Id</th>
-                                    <th>Action</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <?php 
-                                if(count($regclients)>0) {
-                                    foreach ($regclients as $key => $value) {  
-                                ?> 
-                                <tr>
-                                    <td>{{ $value->id ?? "-" }}</td>
-                                    <td>
-                                        <a href="{{url($prefix.'/'.$segment.'/add-regclient-detail/'.Crypt::encrypt($value->id))}}">{{ ucwords($value->name ?? "-")}}</a>
-                                    </td>
-                                    <td>{{$value->location_id ?? "-"}}</td>
-                                    <td>
-                                        <a class="btn btn-primary" href="{{url($prefix.'/regclient-detail/'.Crypt::encrypt($value->id).'/edit')}}"><span><i class="fa fa-edit" aria-hidden="true"></i> Edit<span></a>
-                                        <a class="btn btn-primary" href="{{url($prefix.'/'.$segment.'/view-regclient-detail/'.Crypt::encrypt($value->id))}}"><span><i class="fa fa-eye" aria-hidden="true"></i> View</span></a>
-                                    </td>
-                                </tr>
-                                <?php 
-                                    }
-                                } ?>
-                            </tbody>
-                        </table>
-                        
-                    </div>
-                </div>
+        <div class="page-header layout-spacing">
+            <h2 class="pageHeading">Regional Client List</h2>
+        </div>
+
+
+        <div class="widget-content widget-content-area br-6">
+            <div class="table-responsive mb-4 mt-4">
+                @csrf
+                <table id="clienttable" class="table table-hover get-datatable" style="width:100%">
+                    <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Name</th>
+                        <th>Location Id</th>
+                        <th style="text-align: center">Action</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    @if(count($regclients)>0)
+                        @foreach ($regclients as $key => $value)
+                            <tr>
+                                <td>{{ $value->id ?? "-" }}</td>
+                                <td>
+                                    <a href="{{url($prefix.'/'.$segment.'/add-regclient-detail/'.Crypt::encrypt($value->id))}}">
+                                        {{ ucwords($value->name ?? "-")}}
+                                    </a>
+                                </td>
+                                <td>{{$value->location_id ?? "-"}}</td>
+                                <td>
+                                    <div class="d-flex justify-content-center align-items-center">
+                                        <a class="edit editIcon"
+                                           href="{{url($prefix.'/regclient-detail/'.Crypt::encrypt($value->id).'/edit')}}">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                                 viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                                                 stroke-linecap="round" stroke-linejoin="round"
+                                                 class="feather feather-edit">
+                                                <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path>
+                                                <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path>
+                                            </svg>
+                                        </a>
+                                        <a class="view viewIcon"
+                                           href="{{url($prefix.'/'.$segment.'/view-regclient-detail/'.Crypt::encrypt($value->id))}}">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                                 viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                                                 stroke-linecap="round" stroke-linejoin="round" class="feather feather-eye">
+                                                <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
+                                                <circle cx="12" cy="12" r="3"></circle>
+                                            </svg>
+                                        </a>
+                                    </div>
+                                </td>
+                            </tr>
+                        @endforeach
+                    @endif
+
+                    </tbody>
+                </table>
+
             </div>
         </div>
+
     </div>
 @endsection
