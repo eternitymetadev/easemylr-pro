12f6d7b689646aa0e8d5d73ec6888c9f91b5f7f3
eternitywebdev <dev.eternitysolutions@gmail.com>
2023-01-03 15:34:29 +0530

    task

--- app/Http/Controllers/TechnicalMasterController.php
+++ app/Http/Controllers/TechnicalMasterController.php
@@ -8,6 +8,7 @@ use App\Imports\ItemUpload;
 use App\Models\TechnicalMaster;
 use App\Models\ItemMaster;
 use Excel;
+use Auth;
 
 class TechnicalMasterController extends Controller
 {
@@ -72,6 +73,103 @@ class TechnicalMasterController extends Controller
             $response['success'] = false;
             $response['error'] = true;
             $response['error_message'] = "Can not import item Master please try again";
+        }
+        return response()->json($response);
+    }
+
+    public function addTechnicalName(Request $request)
+    { 
+        $this->prefix = request()->route()->getPrefix();
+         
+        $check_technical_name = TechnicalMaster::where('technical_formula',$request->technical_name)->first();
+        if(!empty($check_technical_name)){
+
+            $response['success_message'] = false;
+            $response['error'] = true;
+            $response['error_message'] = "Name already exists";
+            return response()->json($response);
+        }
+
+        $data =  TechnicalMaster::create(['technical_formula' => $request->technical_name]);
+        $message = "Data imported Successfully";
+
+        if ($data) {
+            $response['success'] = true;
+            $response['error'] = false;
+            $response['success_message'] = $message;
+        } else {
+            $response['success'] = false;
+            $response['error'] = true;
+            $response['error_message'] = "Can not import item Master please try again";
+        }
+        return response()->json($response);
+    }
+
+    public function addItems(Request $request)
+    { 
+        $this->prefix = request()->route()->getPrefix();
+
+            $itemsave['technical_formula'] = $request->technical_formula;
+            $itemsave['erp_mat_code'] = $request->erp_mat_code;
+            $itemsave['manufacturer'] = $request->manufacturer;
+            $itemsave['brand_name']   = $request->brand_name;
+            $itemsave['net_weight'] = $request->net_weight;
+            $itemsave['gross_weight'] = $request->gross_weight;
+            $itemsave['chargable_weight'] = $request->chargable_weight;
+
+        $data =  ItemMaster::create($itemsave);
+        $message = "Data imported Successfully";
+
+        if ($data) {
+            $response['success'] = true;
+            $response['error'] = false;
+            $response['success_message'] = $message;
+        } else {
+            $response['success'] = false;
+            $response['error'] = true;
+            $response['error_message'] = "Can not import item Master please try again";
+        }
+        return response()->json($response);
+    }
+    public function editItemName(Request $request)
+    {
+        
+        $this->prefix = request()->route()->getPrefix();
+        $authuser = Auth::user();
+        $cc = explode(',', $authuser->branch_id);
+
+        $getItem = ItemMaster::where('id', $request->item_id)->first();
+
+        $response['getItem'] = $getItem;
+        $response['success'] = true;
+        $response['message'] = "verified account";
+        return response()->json($response);
+    }
+
+    public function updateItemName(Request $request)
+    {
+        // echo'<pre>'; print_r($request->all()); die;
+        $this->prefix = request()->route()->getPrefix();
+
+            $itemsave['technical_formula'] = $request->technical_formula;
+            $itemsave['erp_mat_code'] = $request->erp_mat_code;
+            $itemsave['manufacturer'] = $request->manufacturer;
+            $itemsave['brand_name']   = $request->brand_name;
+            $itemsave['net_weight'] = $request->net_weight;
+            $itemsave['gross_weight'] = $request->gross_weight;
+            $itemsave['chargable_weight'] = $request->chargable_weight;
+
+        $data =  ItemMaster::where('id', $request->item_id)->update($itemsave);
+        $message = "Item Updated Successfully";
+
+        if ($data) {
+            $response['success'] = true;
+            $response['error'] = false;
+            $response['success_message'] = $message;
+        } else {
+            $response['success'] = false;
+            $response['error'] = true;
+            $response['error_message'] = "Can not update please try again";
         }
         return response()->json($response);
     }
