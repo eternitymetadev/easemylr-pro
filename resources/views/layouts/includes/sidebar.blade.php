bb164c8e0812ae7dfcb87b136fdb465e6b8fbede
eternitywebdev <dev.eternitysolutions@gmail.com>
2023-01-16 15:55:43 +0530

    add app url chge

--- resources/views/layouts/includes/sidebar.blade.php
+++ resources/views/layouts/includes/sidebar.blade.php
@@ -87,9 +87,6 @@
     if(in_array('3', $permissions))
     {
     ?>
-    <li class="menu">
-        <a href="{{$prefixurl.'consigners'}}" data-active="<?php if($segment == 'consigners'){?>true<?php }?>"
-            class="dropdown-toggle">
             <div class="@if(str_contains($currentURL, 'consigners')) active @endif">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
                     stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
