6534c6f0d3321b6ad33344c1a6555a4e005ebee0
eternitywebdev <dev.eternitysolutions@gmail.com>
2023-02-10 16:01:28 +0530

    vendor ch

--- resources/views/vendors/vendor-list.blade.php
+++ resources/views/vendors/vendor-list.blade.php
@@ -75,10 +75,10 @@
                                 <td>-</td>
                                 <?php } ?>
                                 <td><button type="button" class="btn btn-sm btn-primary view"
-                                        value="{{$vendor->id}}">View</button> </td>
-                                        <!-- <a
+                                        value="{{$vendor->id}}">View</button> <a
                                         href="{{ url($prefix.'/edit-vendor/'.$vendor->id) }}"
-                                        class="edit btn btn-sm btn-primary ml-2"><i class="fa fa-edit"></i></a> -->
+                                        class="edit btn btn-sm btn-primary ml-2"><i class="fa fa-edit"></i></a></td>
+                                        
                             </tr>
                             @endforeach
                         </tbody>
