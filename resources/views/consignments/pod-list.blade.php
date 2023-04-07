995fb6ad8cf4b3755c8a9a6ad3d8ad4e62654612
unknown <vikassingh438@gmail.com>
2023-04-07 11:08:27 +0530

    add chg in heading align

--- resources/views/consignments/pod-list.blade.php
+++ resources/views/consignments/pod-list.blade.php
@@ -89,7 +89,7 @@ label.statusLabel {
 }
 </style>
 
-<div class="layout-px-spacing">
+<div class="layout-px-spacing" style="margin-top: 2rem">
     <div class="row layout-top-spacing">
         <div class="col-xl-12 col-lg-12 col-sm-12  layout-spacing">
             <div class="widget-content widget-content-area br-6">
