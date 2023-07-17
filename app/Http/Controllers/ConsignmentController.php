93f06f4a6753d3035c51896bf0f4cf2e7b742019
unknown <vikassingh438@gmail.com>
2023-07-17 16:31:44 +0530

    add return chg

--- app/Http/Controllers/ConsignmentController.php
+++ app/Http/Controllers/ConsignmentController.php
@@ -4690,7 +4690,7 @@ class ConsignmentController extends Controller
     public function getTimelineapi($lr_id)
     {
         try {
-            // $get_delveryrating = EfDeliveryrating::where('lr_id',$lr_id)->first();
+            $get_delveryrating = EfDeliveryrating::where('lr_id',$lr_id)->first();
             $driver_app = DB::table('consignment_notes')->select('consignment_notes.*', 'consignment_notes.job_id as job_id', 'consignment_notes.tracking_link as tracking_link', 'consignment_notes.delivery_status as delivery_status', 'jobs.status as job_status', 'jobs.response_data as trail', 'consigners.postal_code as cnr_pincode', 'consignees.postal_code as cne_pincode','shipto.city as shipto_city', 'locations.name as branch_name', 'fall_in_branch.name as fall_in_branch_name', 'to_branch_name.name as to_branch_detail', 'drivers.name as driver_name')
             ->where('consignment_notes.id', $lr_id)
             ->join('consigners', 'consigners.id', '=', 'consignment_notes.consigner_id')
@@ -4704,7 +4704,7 @@ class ConsignmentController extends Controller
                 $data->on('jobs.consignment_id', '=', 'consignment_notes.id')
                     ->on('jobs.id', '=', DB::raw("(select max(id) from jobs WHERE jobs.consignment_id = consignment_notes.id)"));
             })->first();
-            return ($driver_app);
+            // return ($driver_app);
             if($driver_app){
 
                 $app_trail = json_decode($driver_app->trail, true);
@@ -4721,7 +4721,7 @@ class ConsignmentController extends Controller
                     return response([
                         'data' => $driver_app,
                         'driver_trail' => $result,
-                        // 'delivery_rating' => $get_delveryrating,
+                        'delivery_rating' => $get_delveryrating,
                         'status' => 'success',
                         'code' => 1,
                         'message' => 'Data fetched successfully!',
