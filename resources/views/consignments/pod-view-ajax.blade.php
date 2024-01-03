6a50664511b18df5e316fdbec3f5283223392dcd
unknown <vikassingh438@gmail.com>
2024-01-03 14:57:05 +0530

    add pod upload issue

--- resources/views/consignments/pod-view-ajax.blade.php
+++ resources/views/consignments/pod-view-ajax.blade.php
@@ -40,7 +40,7 @@
                         <label class="lrStatus" style="background: #bb2727">Cancel</label>
                         @elseif ($consignment->status == 1)
                         <label class="lrStatus" style="background: #087408">Active</label>
-                        @elseif ($consignment->status == 2)
+                        @elseif ($consignment->status == 2 || $consignment->status == 6)
                         <label class="lrStatus" style="background: #e2a03f">Unverified</label>
                         @endif
                         <br />
@@ -206,8 +206,8 @@
                         <div class="d-flex justify-content-center flex-wrap" style="gap: 4px; width: 220px; background: #f1f1f1; border-radius: 6px; padding: 5px">
                             Not Available
                         </div> 
-                        <a class="edit @if($consignment->status == 1) editButtonimg @endif editIcon swan-tooltip-left" 
-                        data-tooltip="@if($consignment->status == 1) Add Images @else Need to update status @endif"
+                        <a class="edit @if($consignment->status == 1 && !empty($consignment->vehicle_id)) editButtonimg @endif editIcon swan-tooltip-left" 
+                        data-tooltip="@if($consignment->status == 1 && !empty($consignment->vehicle_id)) Add Images @else Need to update status @endif"
                             data-id="{{$consignment->id}}" lr-date="{{$consignment->consignment_date}}">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
                                 fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
