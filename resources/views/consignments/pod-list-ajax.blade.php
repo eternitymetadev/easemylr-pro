539eadaa8a382523eacdb12d2146d7bd7fc55412
unknown <vikassingh438@gmail.com>
2024-01-03 17:09:14 +0530

    add pod list chg

--- resources/views/consignments/pod-list-ajax.blade.php
+++ resources/views/consignments/pod-list-ajax.blade.php
@@ -175,8 +175,8 @@ $invoice['amt'] = implode(',', $inv_amt);
                             style="gap: 4px; width: 220px; background: #f1f1f1; border-radius: 6px; padding: 5px">
                             Not Available
                         </div>
-                        <a class="edit @if($consignment->status == 1) editButtonimg @endif editIcon swan-tooltip-left"
-                            data-tooltip="@if($consignment->status == 1) Add Images @else Need to update status @endif"
+                        <a class="edit @if($consignment->status == 1  && !empty($consignment->vehicle_id) && $consignment->delivery_status == 'Assigned') editButtonimg @endif editIcon swan-tooltip-left"
+                            data-tooltip="@if($consignment->status == 1  && !empty($consignment->vehicle_id) && $consignment->delivery_status == 'Assigned') Add Images @else Need to update status @endif"
                             data-id="{{$consignment->id}}" lr-date="{{$consignment->consignment_date}}">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
                                 fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
