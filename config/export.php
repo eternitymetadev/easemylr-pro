8e984eaecf9e6ef3719fc1070389cd0abf10fa49
vikaschandel <vikasssingh438@gmail.com>
2024-01-10 13:05:14 +0530

    add mis dispatcher days

--- config/export.php
+++ config/export.php
@@ -2,7 +2,7 @@
 
 return [
     'start_date' => now()->toDateString(),
-    'end_date' => now()->subDays(60)->toDateString(),
+    'end_date' => now()->subDays(45)->toDateString(),
     'base_client_id' => '',
     'reg_client_id' => '',
     'branch_id' => '',
