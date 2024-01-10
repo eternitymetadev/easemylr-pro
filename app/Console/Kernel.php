20f1a266b90c8397a8bcb3d93f0886603ba5a022
vikaschandel <vikasssingh438@gmail.com>
2024-01-10 10:41:42 +0530

    add mis dispat

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -20,6 +20,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         \Log::info('Report2ExportJob started processing...');
+        $sdate = config('export.start_date');
         $schedule->call(function () {
             $sdate = config('export.start_date');
             $edate = config('export.end_date');
@@ -31,7 +32,7 @@ class Kernel extends ConsoleKernel
                 config('export.reg_client_id'),
                 config('export.branch_id')
             );
-        })->dailyAt('10:40');
+        })->dailyAt('10:43');
        \Log::info($sdate);
     }
 
