9495c0234eb509d49ad573d5b02d5f5288cad1d6
vikaschandel <vikasssingh438@gmail.com>
2024-01-11 15:15:25 +0530

    add mis dispatcher working cmd EMAIL ss

--- app/Notifications/ReportExportNotification.php
+++ app/Notifications/ReportExportNotification.php
@@ -38,12 +38,15 @@ class ReportExportNotification extends Notification
      * @param  mixed  $notifiable
      * @return \Illuminate\Notifications\Messages\MailMessage
      */
-    public function toMail($report)
+    public function toMail($notifiable)
     {
+        $path = storage_path('app/public/mis/mis2.xlsx');
         return (new MailMessage)
-                    ->line('Please download the MIS report fot 11-10-2022 to 11-01-2024')
-                    ->attach(url($report))
-                    ->line('Thank you for choosing Shiprider!');
+                    ->line('Please download the MIS report.')
+                    ->attach($path, [
+                        'as' => 'mis2.xlsx',
+                        'mime' => 'xlsx',
+                    ]);
     }
     
 
