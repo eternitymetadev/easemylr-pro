0957b80fd8ca3e9d36034f877e527890cae18279
vikaschandel <vikasssingh438@gmail.com>
2024-01-11 15:05:50 +0530

    add mis dispatcher working cmd EMAIL s

--- app/Notifications/ReportExportNotification.php
+++ app/Notifications/ReportExportNotification.php
@@ -38,12 +38,12 @@ class ReportExportNotification extends Notification
      * @param  mixed  $notifiable
      * @return \Illuminate\Notifications\Messages\MailMessage
      */
-    public function toMail($notifiable)
+    public function toMail($report)
     {
         return (new MailMessage)
-                    ->line('The introduction to the notification.')
-                    ->action('Notification Action', url('/'))
-                    ->line('Thank you for using our application!');
+                    ->line('Please download the MIS report fot 11-10-2022 to 11-01-2024')
+                    ->attach(url($report))
+                    ->line('Thank you for choosing Shiprider!');
     }
     
 
