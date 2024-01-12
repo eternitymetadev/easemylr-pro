fa57afa91c010c1750a816f732bd2a819ae66646
vikaschandel <vikasssingh438@gmail.com>
2024-01-12 11:25:11 +0530

    email temp logo up gg

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,7 +1,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-<img src="{{asset('assets/ridr.png')}}" class="logo" alt="Shiprider Logo">
+<img src="{{asset('assets/ridr.png')}}"  alt="Shiprider Logo">
 </a>
 </td>
 </tr>
