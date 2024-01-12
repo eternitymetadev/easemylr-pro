7ecd48d890c3e218f2d73ca796459266d09f191e
vikaschandel <vikasssingh438@gmail.com>
2024-01-12 11:14:30 +0530

    email temp upsx

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,8 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 @if (trim($slot) === 'Laravel')
-<img src="{{ asset('img/eternity-forwarders-logo.png') }}" class="logo" alt="Laravel Logo">
+<img src="{{asset('assets/img/LOGO_Frowarders.jpg')}}" class="logo" alt="Laravel Logo">
+
 @else
 {{ $slot }}
 @endif
