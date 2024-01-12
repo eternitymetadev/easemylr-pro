48ce09705833e4d094e7c7ad0412146f2dc47a41
unknown <vikassingh438@gmail.com>
2024-01-12 17:18:14 +0530

    add bulk chages

--- app/Imports/DeliveryDateImport.php
+++ app/Imports/DeliveryDateImport.php
@@ -26,7 +26,8 @@ class DeliveryDateImport implements ToModel,WithHeadingRow
         $authuser = Auth::user();
         
         $consignmentNote = ConsignmentNote::find($row['lr_no']);
-        if ($consignmentNote && empty($consignmentNote->delivery_date) && !empty($delivery_date)) {
+        // if ($consignmentNote && empty($consignmentNote->delivery_date) && !empty($delivery_date)) {
+            if ($consignmentNote && !empty($delivery_date)) {
             // dd($consignmentNote);
             $consignmentNote->update([
                 'delivery_date' => $delivery_date,
