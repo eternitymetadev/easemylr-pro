6185960ce95bc059942137abdec23a302fff96cf
vikaschandel <vikasssingh438@gmail.com>
2024-01-12 11:17:16 +0530

    email temp logo

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,12 +1,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-@if (trim($slot) === 'Laravel')
-<img src="{{asset('assets/img/LOGO_Frowarders.jpg')}}" class="logo" alt="Laravel Logo">
-
-@else
-{{ $slot }}
-@endif
+<img src="{{asset('assets/img/LOGO_Frowarders.jpg')}}" class="logo" alt="Shiprider Logo">
 </a>
 </td>
 </tr>
