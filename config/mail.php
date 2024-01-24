7ac7524f4c03e34f0bbdeb3e1075cdfd0a5ac6c9
unknown <vikassingh438@gmail.com>
2024-01-24 11:19:36 +0530

    add chges in config

--- config/mail.php
+++ config/mail.php
@@ -36,8 +36,8 @@ return [
     'mailers' => [
         'smtp' => [
             'transport' => 'smtp',
-            'timeout' => 600,
-            'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
+            'timeout' => 1200,
+            'host' => env('MAIL_HOST', 'smtp.gmail.com'),
             'port' => env('MAIL_PORT', 587),
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
             'username' => env('MAIL_USERNAME'),
