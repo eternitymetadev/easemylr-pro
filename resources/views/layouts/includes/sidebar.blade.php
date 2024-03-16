10fafa844b2acf6957aea29903ef0875e20f1cfe
unknown <vikassingh438@gmail.com>
2024-03-16 15:36:11 +0530

    add sidebar chagess

--- resources/views/layouts/includes/sidebar.blade.php
+++ resources/views/layouts/includes/sidebar.blade.php
@@ -491,12 +491,8 @@
     <li class="menu">
         <a href="#drsPayments" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
             <div
-                class="@if(
-                $currentURL == 'drs-paymentlist' || 
-                $currentURL == 'request-list' || 
-                $currentURL == 'payment-report-view' || 
-                $currentURL == 'prs-payment-report' || 
-                $currentURL == 'drswise-report') active @endif">
+                class="@if($currentURL == 'drs-paymentlist' || $currentURL == 'request-list' || $currentURL == 'payment-report-view' || 
+                $currentURL == 'prs-payment-report' || $currentURL == 'drswise-report') active @endif">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
                     stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
                     class="feather feather-dollar-sign">
